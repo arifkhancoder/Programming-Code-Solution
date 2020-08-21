30614f928626727dbee21503c567d8377f785ac8
Md Ariful Islam <48733739+blackcoder2019@users.noreply.github.com>
2020-08-21 09:58:42 -0700

    Create Drunken Python.py

---
+++ Drunken Python.py
@@ -0,0 +1,13 @@
+def str_to_int(num_str):
+	num = 0
+	for char in num_str:
+		num *= 10
+		num += ord(char) - ord('0')
+	return num
+
+def int_to_str(num_int):
+	word = ""
+	while num_int:
+		word = chr(ord('0') + num_int % 10) + word
+		num_int //= 10
+	return word or "0"
